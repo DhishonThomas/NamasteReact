5880f6d7df120b19dab93d6309f48420ab1527c8
DhishonThomas <dhishonthomas23@gmail.com>
2024-05-05 19:25:49 +0530

    First assinment

---
+++ App.js
@@ -0,0 +1,35 @@
+
+const heading1 = React.createElement(
+   "h1",
+   {key:124,
+      id: "title"
+   },
+   "Heading 1"
+);
+
+const heading2 = React.createElement(
+   "h1",
+   {
+    key:123,
+      id: "title2"
+   }, 
+   "Heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {key:234,
+        id:"container"
+    },
+    [heading1,heading2]
+)
+
+
+
+const root = ReactDOM.createRoot(
+ 
+   document.getElementById("root")
+)
+
+root.render(container)
+
